b31305ce2d68fd8e1a4804f2e26d9e12d7aa5aad
HenryFang <henry@HENRYFANG1838>
2017-08-28 22:34:33 +0800

    第一個綠燈

--- CartLib/Cart.cs
+++ CartLib/Cart.cs
@@ -8,16 +8,47 @@ namespace CartLib
 {
     public class Cart
     {
-        private List<CartItem> carts;
+        private List<CartItem> _carts;
 
         public Cart(List<CartItem> carts)
         {
-            this.carts = carts;
+            this._carts = carts;
         }
 
         public double Calculate()
         {
-            return 0;
+            if (this._carts == null) return 0;
+
+            var discount = (100 - GetDiscount(this._carts.Count)) / 100;
+
+            var price = 0;
+
+            foreach (var item in this._carts)
+            {
+                price += (int)(item.Price * item.Quantity * discount);
+            }
+
+            return price;
+        }
+
+        /// <summary>依傳入的數量傳回折扣數(%)</summary>
+        private double GetDiscount(int count)
+        {
+            switch (count)
+            {
+                case 1:
+                    return 0;
+                case 2:
+                    return 5;
+                case 3:
+                    return 10;
+                case 4:
+                    return 20;
+                case 5:
+                    return 25;
+                default:
+                    return 0;
+            }
         }
     }
 
@@ -25,7 +56,7 @@ namespace CartLib
     public class CartItem
     {
         public int Id { get; set; }
-        public string Name { get; set; }
+        public string BookName { get; set; }
         public double Price { get; set; }
         public int Quantity { get; set; }
     }
--- CartLibTests/CartTests.cs
+++ CartLibTests/CartTests.cs
@@ -15,7 +15,7 @@ namespace CartLib.Tests
         public void CalculateTest_第一集買了一本_其他都沒買_價格應為_100元()
         {
             var carts = new List<CartItem>() {
-                new CartItem() { Id = 1 ,Name = "哈利波特 第一集" ,Price = 100 ,Quantity = 1 }
+                new CartItem() { Id = 1 ,BookName = "哈利波特 第一集" ,Price = 100 ,Quantity = 1 }
             };
 
             var target = new Cart(carts);
